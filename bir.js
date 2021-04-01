69f84a86c4fbe44bc2742562a47ecea2667c8eda
Ilya Fofanov <66513936+Nihaochingiz@users.noreply.github.com>
2021-04-01 22:21:45 +0300

    init
    add birthdayCake function

---
+++ bir.js
@@ -0,0 +1,12 @@
+function birthdayCakeCandles(candles) {
+        var maxHeight = Math.max(...candles);
+        var maxHeightCount = 0;  
+        for(var i = 0; i < candles.length; i++){
+            if (ar[i] == maxHeight){
+                maxHeightCount = maxHeightCount + 1;
+            }
+        }
+        
+        
+    }
+    console.log(maxHeightCount);
