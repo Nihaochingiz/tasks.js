d48ed2a99c9a626fb41fd2998a665685dae15267
Ilya Fofanov <66513936+Nihaochingiz@users.noreply.github.com>
2021-10-27 21:12:35 +0300

    return first element
    Задача: вернуть первый элемент массива

---
+++ firstElementOfArray.js
@@ -0,0 +1,14 @@
+var words = ['Ночь', 'Улица', 'Фонарь', 'Аптека', 'Бессмысленный', 'Тусклый', 'Свет'];
+
+var fruits = ['banana', 'apple', 'peach']
+
+
+getLastElementOfArray(words); // возвращает Ночь;
+
+function getLastElementOfArray(array) {
+  // Напишите код здесь
+return array[0]
+}
+
+
+console.log(getLastElementOfArray(fruits))
