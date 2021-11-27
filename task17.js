7939c2f8bcbc68ffa6edc90a12f6d9459fac0338
Ilya Fofanov <66513936+Nihaochingiz@users.noreply.github.com>
2021-11-27 22:17:33 +0300

    whoseBysicle

--- task17.js
+++ task17.js
@@ -11,67 +11,138 @@ If two or three sons have the same highest marks, you need to choose the younger
 Если 2 или три сына имеют такие же высокие оценки, тебе нужно выбрать самого младшего. Пользуйся таблицей
 */
 
-
+ 
 let firstSon= 
     {
-        'algebra': 6,
-        'history': 8,
-        'physics': 9,
-        'geography': 2,
-        'chemistry': 9
+      'algebra': 1,
+      'history': 1,
+      'physics': 1,
+      'geography': 1,
+      'chemistry': 8
       }
 
       let secondSon= 
       {
-          'algebra': 6,
-          'history': 1,
-          'physics': 2,
-          'geography': 7,
-          'chemistry': 9
+        'algebra': 1,
+        'history': 1,
+        'physics': 1,
+        'geography': 1,
+        'chemistry': 7
         }
         let thirdSon= 
         {
             'algebra': 1,
-            'history': 7,
-            'physics': 3,
-            'geography': 6,
-            'chemistry': 5
+            'history': 1,
+            'physics': 1,
+            'geography': 1,
+            'chemistry': 8
           }
 
       const ageTable = {
-        'firstSonAge': 14,
+        'firstSonAge': 7,
         'secondSonAge': 9,
         'thirdSonAge': 8
       }
-      function whoseBicycle(diary1,diary2,diary3) {
-       
-        var total1 = 0;
-        var total2 = 0;
-        var total3 = 0;
-         for ( var key1 in diary1) {
-           total1 += diary1[key1];
-         }
-        
-       
-        for ( var key2 in diary2) {
-          total2 += diary2[key2];
-          
-        }
-      
-       for ( var key3 in diary3) {
-        total3 += diary3[key3]; 
-      }
 
-      console.log(total1);
-      console.log(total2);
-      console.log(total3);
       
-    
+     
+
+    //Переводим объект ageTable в массив
+    //Далее ищем самый маленький элемент массива
+//let age = Object.values(ageTable);
+//let min = Math.min(...age);
+function findMin (obj) {
+  let min = 2000;
+  let son;
+  for ( let key in obj ) {
+      if ( obj[key] < min) {
+          min = obj[key];
+          son = key;
+      }
+    }
+    son = String(son);
+    console.log(typeof son);
+    if(son === 'firstSonAge'){
+      return "I need to buy a bicycle for my first son.";
+       }else  if(son === 'secondSonAge'){
+         return "I need to buy a bicycle for my second son.";
+       }else if(son === 'thirdSonAge'){
+         return "I need to buy a bicycle for my third son.";
+       }
     }
 
-
+     //В данной функции сравниваем сумму оценок из дневников
+    function whoseBicycle(diary1,diary2,diary3) {
+       
+      var total1 = 0;
+      var total2 = 0;
+      var total3 = 0;
+       for ( var key1 in diary1) {
+         total1 += diary1[key1];
+       }
+      
+     
+      for ( var key2 in diary2) {
+        total2 += diary2[key2];
         
+      }
+    
+     for ( var key3 in diary3) {
+      total3 += diary3[key3]; 
+    }
+    let arr = {'firstSonAge':total1,'secondSonAge':total2,'thirdSonAge':total3};
+    let max = arr['firstSonAge'];
+    let keyMax;
+    for(let key in arr){
+    if(max< arr[key]){
+      max = arr[key];
+      keyMax = key;
+    }
+    }
+    let arrmax ={};
+    for(let key in arr){
+      if(max === arr[key]){
+        arrmax = {...arrmax,[key]: ageTable[key]};
+      }
+      }
+if(Object.values(arrmax).length>1){
+  console.log(1)
+  return findMin(arrmax)
+}else{
+      let son = String(Object.keys(arrmax)[0]);
+  if(son === 'firstSonAge'){
+ return "I need to buy a bicycle for my first son.";
+  }else  if(son  === 'secondSonAge'){
+    return "I need to buy a bicycle for my second son.";
+  }else if(son === 'thirdSonAge'){
+    return "I need to buy a bicycle for my third son.";
+  }
+}
 
-      console.log(whoseBicycle(firstSon,secondSon,thirdSon));
+
+
+      /*if(total1 === total2){
+        findMin({'firstSonAge': ageTable['firstSonAge'],'secondSonAge':ageTable['secondSonAge']});
+      }else  if(total1 === total3){
+        findMin({'firstSonAge': ageTable['firstSonAge'],'thirdSonAge':ageTable['thirdSonAge']});
+      }else  if(total2 === total3){
+        findMin({'secondSonAge': ageTable['secondSonAge'],'thirdSonAge':ageTable['thirdSonAge']});
+      }else  if(total1 === total2 && total2 === total3){
+        findMin(ageTable);
+      }else{
+
+      }*/
+   
+    
+   // result = [total1, total2,total3]
+    
+  
+      
+
+
+  }
+ 
 
       
+  console.log(whoseBicycle(firstSon,secondSon,thirdSon));
+  
