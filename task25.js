8d514934638bc01470e173f910e54a328ec663e7
Ilya Fofanov <66513936+Nihaochingiz@users.noreply.github.com>
2021-12-07 13:58:29 +0300

    min

---
+++ task25.js
@@ -0,0 +1,19 @@
+/*
+Напишите функцию, которая принимает два аргумента и возвращает минимальный
+*/
+
+function min(a,b) {
+    let minValue = 0;
+    if (a > b) {
+        return minValue+=b;
+    }
+    else {
+        return minValue+=a;
+    }
+
+
+}
+
+
+console.log(min(5,22));
+console.log(min(88,33));
