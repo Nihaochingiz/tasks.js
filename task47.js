e91ea8b34615216fced6a892e583e5f718ab170d
Ilya Fofanov <66513936+Nihaochingiz@users.noreply.github.com>
2021-12-20 17:30:16 +0300

    Add files via upload

---
+++ task47.js
@@ -0,0 +1,37 @@
+/*
+Задание
+Реализуйте функцию convertText(), которая принимает на вход строку и, если первая буква не заглавная, возвращает перевернутый вариант исходной строки. Если первая буква заглавная, то строка возвращается без изменений. Если на вход передана пустая строка, функция должна вернуть пустую строку.
+
+Примеры вызова:
+
+convertText('Hello'); // 'Hello'
+convertText('hello'); // 'olleh'
+
+// Не забудьте учесть пустую строку!
+convertText(''); // ''
+Перевернуть строчку можно используя функцию reverse(). В качестве аргумента в неё нужно передать строку, которую мы хотим перевернуть:
+
+const result = reverse('Hello!');
+console.log(result); // => '!olleH'
+*/
+
+
+
+const convertText = (string)=> {
+    const firstChar = string[0];
+    
+    if ( firstChar == firstChar.toUpperCase()) {
+        return  string.split("").reverse().join("");
+       
+    }
+    else if (firstChar === string[0]) {
+        return string;
+        
+   
+    
+    }
+}
+
+
+console.log(convertText('hello'));
+console.log(convertText('Hello'));
