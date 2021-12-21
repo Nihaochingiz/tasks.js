2976d2685b0b1c37dec4a5907dddda9d7505cd80
Ilya Fofanov <66513936+Nihaochingiz@users.noreply.github.com>
2021-12-21 11:14:16 +0300

    reverseString

---
+++ reverseString.js
@@ -0,0 +1,13 @@
+
+let word = 'hello';
+
+let reverseString=(str)=> {
+    let result = '';
+    for ( let i = str.length-1; i >= 0; i-- ){
+        result += str[i]
+    }
+return result;
+}
+
+
+console.log(reverseString(word));
