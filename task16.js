4766201bc3d184cee9ecf25d9a20184d310d0a8b
Ilya Fofanov <66513936+Nihaochingiz@users.noreply.github.com>
2022-04-29 11:48:14 +0300

    findSquares

--- task16.js
+++ task16.js
@@ -1,6 +1,7 @@
 
 /*
-Complete the function that takes an odd integer (0 < n < 1000000) which is the difference between two consecutive perfect squares, and return these squares as a string in the format "bigger-smaller"
+Complete the function that takes an odd integer 
+(0 < n < 1000000) which is the difference between two consecutive perfect squares, and return these squares as a string in the format "bigger-smaller"
 */
 
 
